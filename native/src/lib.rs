fcdbfc96c869464a38c1c3321c50f99190260715
hazzard <thehazzard@gmail.com>
2018-04-24 19:13:42 -0300

    feat(progres): s

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,13 +1,13 @@
 #[macro_use]
 extern crate neon;
 
-use std::ops::DerefMut;
 use neon::mem::Handle;
+use neon::ops::Try;
 use neon::vm::{Call, JsResult};
-use neon::js::{JsArray, Object, Value, JsValue};
+use neon::js::{JsArray, Object, JsValue, JsString};
 
 
-fn cartesian_product_of(call: Call) -> JsResult<JsValue> {
+fn cartesian_product_of(call: Call) -> JsResult<JsString> {
     let scope = call.scope;
     let args = call.arguments;
     let args_len = args.len();
@@ -19,17 +19,28 @@ fn cartesian_product_of(call: Call) -> JsResult<JsValue> {
         let a = args.get(scope, index).unwrap().check::<JsArray>()?;
         arguments.push(a);
     }
+    let mut arr: Vec<Vec<Handle<_>>> = Vec::new();
+    arguments.iter().fold(arr, | acc, &array | {
+        let mut local_acc: Vec<Vec<Handle<JsValue>>> = Vec::new();
 
-    let results = match arguments.split_first() {
-        Some((first, rest)) => {
-            
-        },
-        None => {
-            vec![]
+        for acc_item in local_acc {
+            let mut s = 0;
+            for a in 0..array.len() {
+                let elem1 = array.get(scope, a).unwrap().check::<JsArray>().unwrap().to_vec(scope);
+                acc_item.append(&mut elem1.unwrap());
+                let jsarray = JsArray::new(scope, acc_item.len() as u32).check::<JsArray>().unwrap();
+                let jsarray = acc_item.into_iter().enumerate().fold(jsarray, |acc, (i, elem)| {
+                    acc.set(i as u32, elem);
+                    acc
+                });
+                local_acc.push(acc_item);
+                println!("{}", 'a');
+            }
         }
-    };
+        local_acc
+    });
 
-    Ok(finalArray.as_value(scope))
+    Ok(JsString::new(scope, "hola").unwrap())
 }
 
 register_module!(m, {
