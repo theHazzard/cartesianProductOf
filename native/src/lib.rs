054e34174e4b5fae0596a1e46b14f1835359d7e9
hazzard <thehazzard@gmail.com>
2018-04-24 00:49:37 -0300

    progress

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,14 +1,44 @@
 #[macro_use]
 extern crate neon;
 
+use neon::mem::{Handle};
+use neon::scope::Scope;
 use neon::vm::{Call, JsResult};
-use neon::js::JsString;
+use neon::js::{JsString, JsArray, Object, JsObject};
 
-fn hello(call: Call) -> JsResult<JsString> {
+fn jsarray_as_vector<'a, T: Scope<'a>> (scope: &mut T, jarray: &JsArray) -> Vec<JsObject> {
+    let vector: Vec<Handle<JsObject>> = Vec::new();
+
+    for elem in 0..jarray.len() {
+        let newElem: JsObject = jarray.get(scope, elem).unwrap().deref();
+        vector.push(newElem);
+    }
+    vector
+}
+
+fn cartesian_product_of(call: Call) -> JsResult<JsString> {
     let scope = call.scope;
-    Ok(JsString::new(scope, "hello node").unwrap())
+    let raw_args = call.arguments;
+    let args_len = raw_args.len();
+
+    let mut result:Vec<Vec<JsArray>> = Vec::new();
+    result.push(Vec::new());
+
+    for i in 0..args_len {
+        let mut result: Vec<Vec<JsArray>> = Vec::new();
+        for _elem in result.iter() {
+            let arg_elements: Handle<JsArray> = raw_args.get(scope, i).unwrap();
+
+            for l in 0..arg_elements.len() {
+                let item_elem = arg_elements.get(scope, l).unwrap().check::<JsArray>().unwrap();
+
+                let arr = jsarray_as_vector(scope, &item_elem);
+            }
+        }
+    }
+    Ok(JsString::new(scope, "Hola").unwrap())
 }
 
 register_module!(m, {
-    m.export("hello", hello)
+    m.export("cartesianProductOf", cartesian_product_of)
 });
